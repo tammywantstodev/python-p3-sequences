4468893b04903c4cf1384955bb1b539650dc1b82
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2025-02-12 18:45:27 +0300

    saving changes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+
+
 def print_fibonacci(length):
-    pass
+    if length==0:
+        print([])
+    elif length==1:
+        print([0])
+    else:
+        list_of_numbers=[0,1]
+        for _ in range(length-2):
+            next_number= list_of_numbers[-1]+list_of_numbers[-2]
+            list_of_numbers.append(next_number)
+        print(list_of_numbers)
+print_fibonacci(0)
+#[]
+print_fibonacci(1)
+#0
